70a168b273728bacb64f1b7cb0407c52cb911a7a
Vik <viktoria.om@gmail.com>
2025-11-14 20:21:17 +0200

    register all models in admin

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from social.models import (
+    Profile,
+    Post,
+    Follow,
+    Like,
+    Hashtag,
+    Comment
+)
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Follow)
+admin.site.register(Like)
+admin.site.register(Hashtag)
+admin.site.register(Comment)
