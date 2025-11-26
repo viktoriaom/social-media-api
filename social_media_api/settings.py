d2c883adecd3767a9d22053111a78a35abed39ac
Vik <viktoria.om@gmail.com>
2025-11-26 23:41:39 +0200

    add static & media roots and urls

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
